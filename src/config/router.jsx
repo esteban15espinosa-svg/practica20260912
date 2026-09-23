ed10847d9b268d8f415faca6f58e33dfe00b459e
Otoniel Esteban Espinoza Contreras <esteban15espinosa@gmail.com>
2026-09-23 00:23:13 -0600

    Configuracion despliegue github pages

--- src/config/router.jsx
+++ src/config/router.jsx
@@ -51,4 +51,6 @@ export const router = createBrowserRouter([
         element: <Navigate to="/estudiantes" replace />,
       },
 
-]);
+], {
+    basename: "/practica20260912" // <--- Agrega este segundo parámetro aquí
+  });
